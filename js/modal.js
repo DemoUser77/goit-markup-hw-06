2bbc6d8d6ca63da6e63a3648b75cf6269f8e2155
DemoUser77 <katykatykaty001@gmail.com>
2022-11-19 00:00:21 +0200

    change

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    modalWindow: document.querySelector("[data-modal-window]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,6 +11,7 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.modalWindow.classList.toggle("modal-animation");
     
   }
 })();
